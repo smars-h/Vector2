4a61fb62be5cf3157554e9ba2c472a0cadcf13b1
smars-h <matakupresak@gmail.com>
2021-02-22 17:47:58 +0100

    Fix

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const Vector = function (x, y) {
-    this.x = x || 0
-    this.y = y || 0
-}
+    this.x = x || 0;
+    this.y = y || 0;
+};
 
-Vector.prototype.add = function (n, j) {
+Vector.prototype.add = function (n, j = n) {
     if (n instanceof Vector) {
         return new Vector(this.x + n.x, this.y + n.y);
     } else {
         return new Vector(this.x + n, this.y + j);
     }
-}
+};
 
-Vector.prototype.sub = function (n, j) {
+Vector.prototype.sub = function (n, j = n) {
     if (n instanceof Vector) {
         return new Vector(this.x - n.x, this.y - n.y);
     } else {
-        return new Vector(this.y - n, this.y - j);
+        return new Vector(this.x - n, this.y - j);
     }
-}
+};
 
-Vector.prototype.mult = function (n, j) {
+Vector.prototype.mult = function (n, j = n) {
     if (n instanceof Vector) {
         return new Vector(this.x * n.x, this.y * n.y);
     } else {
-        return new Vector(this.y * n, this.y * j);
+        return new Vector(this.x * n, this.y * j);
     }
-}
+};
 
-Vector.prototype.div = function (n, j) {
+Vector.prototype.div = function (n, j = n) {
     if (n instanceof Vector) {
         return new Vector(this.x / n.x, this.y / n.y);
     } else {
-        return new Vector(this.y / n, this.y / j); 
+        return new Vector(this.x / n, this.y / j);
     }
-}
+};
 
 Vector.prototype.len = function () {
-    return Math.sqrt(this.x ** 2 + this.y ** 2); 
-}
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+};
 
 Vector.prototype.norm = function () {
     return this.div(this.len());
-}
+};
 
 module.exports = Vector;
